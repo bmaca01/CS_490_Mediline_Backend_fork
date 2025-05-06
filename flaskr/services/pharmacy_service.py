dd5f8a9cb2e79e027c07f2f982c70df1cee88f1c
Ben Macaro <bjm57@njit.edu>
2025-05-06 16:03:56 -0400

    send with kombu attempt

--- flaskr/services/pharmacy_service.py
+++ flaskr/services/pharmacy_service.py
@@ -41,15 +41,74 @@ def get_all_pharmacy_patients(pharmacy_id, new_request_time=datetime.now() - tim
 def add_pt_rx(pharmacy_id, patient_id, doctor_id, medications):
     # TODO: detect duplicates / make idempotent
     try:
-        res: AsyncResult = current_app.extensions['celery'].send_task(
-            "send_rx", 
-            kwargs={
-                'pharmacy_id': pharmacy_id, 
-                'patient_id': patient_id, 
-                'doctor_id': doctor_id, 
-                'medications': medications
-            }
-        )
+        if current_app.config['FLASK_ENV'] in {'prod', 'production'}:
+            import os, uuid
+            from kombu import Connection, Producer, Exchange
+            with Connection(os.getenv('QUEUE_URL')) as conn:
+                with conn.channel() as channel:
+                    prod = Producer(channel)
+                    exchange = Exchange(
+                        'prescription_queue', 
+                        type='direct'
+                    )
+                    """
+                        body=json.dumps({
+                            'pharmacy_id': pharmacy_id, 
+                            'patient_id': patient_id, 
+                            'doctor_id': doctor_id, 
+                            'medications': medications
+                        }),
+                        headers={
+                            'lang': 'py',
+                            'task': 'flaskr.tasks.send_rx',
+                            'argsrepr': repr(args),
+                            'kwargsrepr': repr(kwargs)
+
+                        }
+                    """
+                    task_id = uuid()
+                    prod.publish(
+                        correlation_id=task_id,
+                        retry=True,
+                        retry_policy={
+                            'interval_start': 0,
+                            'interval_step': 2,
+                            'interval_max': 30,
+                            'max_retries': 10
+                        },
+                        exchange='prescription_queue',
+                        routing_key='prescription_queue',
+                        serializer='msgpack',
+                        body=(
+                            [],
+                            {
+                                'pharmacy_id': pharmacy_id, 
+                                'patient_id': patient_id, 
+                                'doctor_id': doctor_id, 
+                                'medications': medications
+                            },
+                            {}
+                        ),
+                        headers={
+                            'id': task_id,
+                            'lang': 'py',
+                            'task': 'flaskr.tasks.send_rx',
+                            'root_id': None,
+                            'parent_id': None,
+                            'group': None,
+                        }
+                    )
+
+        else:
+            res: AsyncResult = current_app.extensions['celery'].send_task(
+                "send_rx", 
+                kwargs={
+                    'pharmacy_id': pharmacy_id, 
+                    'patient_id': patient_id, 
+                    'doctor_id': doctor_id, 
+                    'medications': medications
+                }
+            )
         """
         res: AsyncResult = send_rx.apply_async(
             kwargs={
