b73f233d50a4d29a859cdbab18c55c24a5cba547
Ben Macaro <bjm57@njit.edu>
2025-05-06 17:26:00 -0400

    kombu attempt 2

--- flaskr/services/pharmacy_service.py
+++ flaskr/services/pharmacy_service.py
@@ -42,17 +42,49 @@ def add_pt_rx(pharmacy_id, patient_id, doctor_id, medications):
     # TODO: detect duplicates / make idempotent
     try:
         if current_app.config['FLASK_ENV'] in {'prod', 'production'}:
-            from flaskr.main import celery
-            print('sending task')
-            celery.send_task(
-                "flaskr.tasks.send_rx", 
-                kwargs={
-                    'pharmacy_id': pharmacy_id, 
-                    'patient_id': patient_id, 
-                    'doctor_id': doctor_id, 
-                    'medications': medications
-                }
-            )
+            import os
+            from kombu import Connection, Producer, Exchange
+            from kombu.utils import uuid
+            with Connection(os.getenv('QUEUE_URL')) as conn:
+                with conn.channel() as channel:
+                    prod = Producer(channel)
+                    exchange = Exchange(
+                        'prescription_queue', 
+                        type='direct'
+                    )
+                    task_id = uuid()
+                    try:
+                        prod.publish(
+                            {
+                                'args': [],
+                                'kwargs': {
+                                    'pharmacy_id': pharmacy_id, 
+                                    'patient_id': patient_id, 
+                                    'doctor_id': doctor_id, 
+                                    'medications': medications
+                                }
+                            },
+                            correlation_id=task_id,
+                            retry=True,
+                            retry_policy={
+                                'interval_start': 0,
+                                'interval_step': 2,
+                                'interval_max': 30,
+                                'max_retries': 10
+                            },
+                            exchange=exchange,
+                            routing_key='prescription_queue',
+                            headers={
+                                'id': task_id,
+                                'lang': 'py',
+                                'task': 'flaskr.tasks.send_rx',
+                                'root_id': None,
+                                'parent_id': None,
+                                'group': None,
+                            }
+                        )
+                    except Exception as e:
+                        raise e
         else:
             res: AsyncResult = current_app.extensions['celery'].send_task(
                 "send_rx", 
@@ -81,3 +113,34 @@ def get_pharmacy_info(pharmacy_id):
     if not pharmacy:
         return None
     return pharmacy.to_dict()
+
+def foo():
+    # old attempts
+    """
+from flaskr.main import celery
+print('sending task')
+celery.send_task(
+"flaskr.tasks.send_rx", 
+kwargs={
+    'pharmacy_id': pharmacy_id, 
+    'patient_id': patient_id, 
+    'doctor_id': doctor_id, 
+    'medications': medications
+},
+queue='prescription_queue'
+)
+        body=json.dumps({
+            'pharmacy_id': pharmacy_id, 
+            'patient_id': patient_id, 
+            'doctor_id': doctor_id, 
+            'medications': medications
+        }),
+        headers={
+            'lang': 'py',
+            'task': 'flaskr.tasks.send_rx',
+            'argsrepr': repr(args),
+            'kwargsrepr': repr(kwargs)
+
+        }
+    """
+    pass
